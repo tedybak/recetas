6e89321ecd704e95fcbfba27be74e471f1747a59
= <=>
2020-02-11 08:17:09 +0000

    in progress

--- src/app/core/shell/components/details/details.component.ts
+++ src/app/core/shell/components/details/details.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute, Params } from "@angular/router";
 import { RecetaService } from '../../services/receta.service';
 import { IReceta } from '../../interfaces/ireceta';
-import { AuthService} from '../../services/auth.service';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: "app-details",
@@ -10,32 +10,49 @@ import { AuthService} from '../../services/auth.service';
   styleUrls: ["./details.component.scss"]
 })
 export class DetailsComponent implements OnInit {
-  id: string;
+  idReceta: string;
   receta;
-  userId;
-  isOwner: boolean; //con esta propiedd comprobamos que el usuario es el owner de la receta
+  userLoggedId;
+  userRecetaId;
+  isOwner: boolean = false; //con esta propiedd comprobamos que el usuario es el owner de la receta
+
   constructor(
     private route: ActivatedRoute,
     private recetaService: RecetaService,
-    private authService : AuthService
-    
-    ) {}
+    private authService: AuthService
+
+  ) { }
 
   ngOnInit() {
-    this.id = this.route.snapshot.paramMap.get("id");
-    this.receta = this.recetaService.getReceta(this.id).subscribe(data =>{
-      this.receta = data;
-    });
-    this.userId = this.authService.getAuth().subscribe( usuario =>{
-      this.userId = usuario;
-    })
 
-    if ( this.receta.userId == this.userId.uid) {
-      this.isOwner = true;
-    }
-
-  }
-  onClickDelete() {
     
+    this.datosUsuarioLoggued();
+    this.datosReceta();
+
   }
+
+  datosUsuarioLoggued() {
+    this.userLoggedId = this.authService.getAuth().subscribe(usuario => {
+      this.userLoggedId = usuario.uid;     
+    })
+  }
+  datosReceta() {
+    this.idReceta = this.route.snapshot.paramMap.get("id");
+    this.receta = this.recetaService.getReceta(this.idReceta).subscribe(data => {
+      this.userRecetaId = data.userId;
+      this.receta = data;
+
+      if(this.userLoggedId == this.userRecetaId){
+        this.isOwner = true;
+      } 
+
+    });
+  }
+
+
+  onClickDelete() {
+
+  }
+
+
 }
